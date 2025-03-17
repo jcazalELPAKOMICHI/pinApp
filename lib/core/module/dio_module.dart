d3fc50ea022ddef34050082404f96d8c7a3025c0
jose-cazal-itti <jose.cazal@itti.cazal>
2025-03-17 09:57:31 -0300

    agregamos intercepror en Dio

--- lib/core/module/dio_module.dart
+++ lib/core/module/dio_module.dart
@@ -2,21 +2,47 @@ import 'package:injectable/injectable.dart';
 import 'package:dio/dio.dart';
 import 'package:pin_app/core/http/ic_config.dart';
 import 'package:pin_app/core/interceptors/internet_checker_interceptor.dart';
+import 'package:logger/logger.dart';
 
 @module
 abstract class DioModule {
   @singleton
   Dio dio(IConfig config) {
-    Dio dio = Dio(
-      BaseOptions(
-        baseUrl: config.baseUrl,
-        connectTimeout: const Duration(seconds: 60),
-        sendTimeout: const Duration(seconds: 60),
-        receiveTimeout: const Duration(seconds: 60),
-        headers: config.headers,
-      ),
-    )..interceptors.add(InternetCheckerInterceptor());
+    Dio dio =
+        Dio(
+            BaseOptions(
+              baseUrl: config.baseUrl,
+              connectTimeout: const Duration(seconds: 60),
+              sendTimeout: const Duration(seconds: 60),
+              receiveTimeout: const Duration(seconds: 60),
+              headers: config.headers,
+            ),
+          )
+          ..interceptors.add(InternetCheckerInterceptor())
+          ..interceptors.add(DioInterceptor());
 
     return dio;
+  }
+}
+
+class DioInterceptor extends Interceptor {
+  var logger = Logger();
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    super.onRequest(options, handler);
+    logger.t(options.data.toString());
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    super.onResponse(response, handler);
+    logger.i(response.toString());
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    super.onError(err, handler);
+    logger.e(err.type.name, error: err.response.toString());
   }
 }
--- test/client_test.dart
+++ test/client_test.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_test/flutter_test.dart' as test;
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:injectable/injectable.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mockito/mockito.dart' as mock;
 import 'package:pin_app/core/injectable/config.dart';
 import 'package:pin_app/features/post/data/model/post_model.dart';
 import 'package:pin_app/features/post/data/repositories/remote_repository.dart';
@@ -9,20 +9,20 @@ import 'package:pin_app/features/post/domain/repositories/post_repository.dart';
 
 @Environment("test")
 @Injectable(as: PostRepository)
-class MockRemoteRepository extends Mock implements RemoteRepository {}
+class MockRemoteRepository extends mock.Mock implements RemoteRepository {}
 
 void main() {
   late MockRemoteRepository repository;
 
-  test.setUpAll(() async {
+  setUpAll(() async {
     configureDependencies();
     TestWidgetsFlutterBinding.ensureInitialized();
 
     repository = MockRemoteRepository();
   });
 
-  test.group('get posts', () {
-    test.test('Retornando posts', () async {
+  group('get posts', () {
+    test('Retornando posts', () async {
       Future<List<PostModel>> getList() async {
         final List<PostModel> posts = List.filled(
           10,
